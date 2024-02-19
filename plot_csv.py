3a1aff6634bf5cbf6a1a00e87f64d3d1de843002
lkiely1 <144849654+lkiely1@users.noreply.github.com>
2024-02-19 11:29:44 +0000

    input vaidation added, made it so 1 plot can be shown too (before had to be >= 2)

--- plot_csv.py
+++ plot_csv.py
@@ -13,7 +13,7 @@ from pathlib import Path
 
 from edf_to_csv import edf_to_csv
 
-filename = 'S001R01.edf'
+filename = 'chb01_01.edf'
 # note, for some reason run window always shows
 # "Extracting EDF parameters from C:\Users\R00212290\Desktop\Research Project\EEG-Viewer-Annotator\S001R01.edf..."
 # ask why
@@ -31,7 +31,7 @@ check_csv_file = os.path.isfile(csv_path)
 check_edf_file = os.path.isfile(edf_path)
 check_txt_file = os.path.isfile(txt_path)
 
-if check_csv_file:
+if check_csv_file and check_txt_file:
     print("CSV file already exists, opening")
 elif check_edf_file:
     print("CSV file doesn't exist for this EDF file, converting")
@@ -89,14 +89,18 @@ x = True
 
 while x:
     add = ''
-    col_num = 0
-    while col_num < 1 or col_num >= len(col_list):
-        col_num = int(input(f"Select channel to plot (between 1 and {len(col_list) - 1}) -> "))
-        if col_num < 1 or col_num > len(col_list):
-            print("Invalid input")
+    while col_num < 1 or col_num > len(col_list) - 1:
+        try:
+            col_num = int(input(f"Select channel to plot (between 1 and {len(col_list) - 1}) -> "))
+        except:
+            print('Please enter number for channel.')
+            continue
+        if col_num < 1 or col_num > len(col_list) - 1:
+            print(f"Invalid input. {col_num} is not a valid channel number")
+            continue
         elif csv_data.columns[col_num] in col_names:
             print("Channel already selected. Choose another")
-            col_num = 0
+            continue
 
         if col_num != 0:
             col_names.append(csv_data.columns[col_num])
@@ -106,9 +110,10 @@ while x:
         add = str(input("Do you want to add another channel? (y/n) -> ")).lower().strip()
         if add != "y" and add != "n":
             print("Please say yes (y) or no (n)")
+        elif add == 'y':
+            col_num = 0
         elif add == 'n':
             x = False
-
 
 file_length = len(csv_data) / time_freq
 print(f"EEG file is {file_length} seconds long")
@@ -122,19 +127,27 @@ mode = 0
 
 def start_time(file_length, time_amount):
     min_time = -1
-    while min_time < 0 or min_time > file_length - time_amount:
-        min_time = int(input(f"Select start time to plot: "))
+    while min_time < 0 or min_time > file_length:
+        try:
+            min_time = int(input(f"Select start time to plot: "))
+        except:
+            print('Please enter a number for start time.')
+            continue
         if min_time < 0 or min_time > file_length:
-            print("Invalid input")
+            print(f"Invalid input. Pick number between range {0} and {file_length}")
     return min_time
 
 
 def end_time(file_length, min_time):
     max_time = -1
-    while max_time <= 0 or max_time > file_length:
-        max_time = int(input(f"Select end time to plot: "))
+    while max_time <= 0 or max_time > file_length or max_time < min_time:
+        try:
+            max_time = int(input(f"Select end time to plot: "))
+        except:
+            print('Please enter a number for end time.')
+            continue
         if max_time <= 0 or max_time > file_length or max_time < min_time:
-            print("Invalid input")
+            print(f"Invalid input. Pick number between range {min_time} and {file_length} ")
     return max_time
 
 
@@ -143,13 +156,21 @@ print("Select mode")
 print("1 - Set amount of time")
 print("2 - Set min and max time")
 while mode == 0 or mode > 2:
-    mode = int(input("-> "))
+    try:
+        mode = int(input("-> "))
+    except:
+        print('Please enter 1 or 2.')
+        continue
 
     if mode == 1:
         while time_amount <= 0 or time_amount > file_length:
-            time_amount = int(input(f"Select amount of time to plot : "))
+            try:
+                time_amount = int(input(f"Select amount of time to plot : "))
+            except:
+                print('Please enter a number of time.')
+                continue
             if time_amount <= 0 or time_amount > file_length:
-                print("Invalid input")
+                print(f"Invalid input. Pick number less than or equal to {file_length}")
             elif time_amount == file_length:
                 print("Plotting full data")
                 min_time = 0
@@ -158,40 +179,53 @@ while mode == 0 or mode > 2:
                 min_or_max = 0
                 while min_or_max == 0 or min_or_max > 2:
                     print(f"Set 1: start time (min possible 0) or 2: end time (max possible {file_length})")
-                    min_or_max = int(input("-> "))
+                    try:
+                        min_or_max = int(input("-> "))
+                    except:
+                        print('Invalid input. Please enter 1 or 2.')
+                        continue
                     if min_or_max == 0 or min_or_max > 2:
-                        print("Invalid input")
+                        print("Please enter 1 or 2")
                     elif min_or_max == 1:
                         min_time = start_time(file_length, time_amount)
                         max_time = min_time + time_amount
                     elif min_or_max == 2:
-                        min_time = 0
+                        min_time = time_amount
                         max_time = end_time(file_length, min_time)
                         min_time = max_time - time_amount
 
     elif mode == 2:
         min_time = start_time(file_length, time_amount)
         max_time = end_time(file_length, min_time)
+    else:
+        print("Please enter 1 or 2")
 
-# want to let user chose num of subplots
-# get data for each selected channel
-# loop through array/list whatever for each plot
-# will try thursday
 
-fig, axs = plt.subplots(len(col_names), sharey=True)
+fig, axs = plt.subplots(len(col_names), sharex=True, sharey=True)
 
 plt.rcParams['lines.linewidth'] = 0.3
 
 fig.suptitle(f"EEG Graph for {col_names}")
 
-for i in range(len(col_names)):
-    axs[i].plot(csv_data.time, csv_data[col_names[i]], 'tab:blue')
-    axs[i].set(ylabel=f"{col_names[i]}")
+if len(col_names) > 1:
+    for i in range(len(col_names)):
+        axs[i].plot(csv_data.time, csv_data[col_names[i]], 'tab:blue')
+        axs[i].set(ylabel=f"{col_names[i]}")
 
-for ax in axs.flat:
-    ax.set_xlim(min_time, max_time) # need to get min max ylims
+    for ax in axs.flat:
+        ax.set_xlim(min_time, max_time)  # need to get min max ylims
 
-axs[-1].set(xlabel="Time (S)")
-#axs.set(ylabel="Amplitude/Voltage (uV)")
+    axs[-1].set(xlabel="Time (S)")
+    # axs.set(ylabel="Amplitude/Voltage (uV)")
+
+else: # if only 1 in plot
+    print("only 1 channel in plot")
+    axs.plot(csv_data.time, csv_data[col_names[0]], 'tab:blue')
+    axs.set(ylabel=f"{col_names[0]}")
+
+    axs.set_xlim(min_time, max_time)  # need to get min max ylims
+
+    axs.set(xlabel="Time (S)")
+    axs.set(ylabel="Amplitude/Voltage (uV)")
 
 plt.show()
