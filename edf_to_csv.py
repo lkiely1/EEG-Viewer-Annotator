0e471ed343bbec8f928c4a57ad74a8828222442c
lkiely1 <144849654+lkiely1@users.noreply.github.com>
2024-02-22 12:23:00 +0000

    added filepickers to gui instead of just text fields

--- edf_to_csv.py
+++ edf_to_csv.py
@@ -1,18 +1,19 @@
 import mne
 
-
-def edf_to_csv(filename): # way to make sure file is edf to prevent crash?
+def edf_to_csv(filename):
     if filename.endswith('.edf'):
-        filename = filename.replace('.edf', '')
-    data_raw_file = (filename + '.edf')
-    raw = mne.io.read_raw_edf(data_raw_file)
-    df = raw.to_data_frame()
-    df.to_csv(filename + '.csv', index=False)
+        data_raw_file = (filename)
+        raw = mne.io.read_raw_edf(data_raw_file)
+        df = raw.to_data_frame()
+        df.to_csv(filename + '.csv', index=False)
 
-    sfreq = raw.info['sfreq']
+        sfreq = raw.info['sfreq']
 
-    with open(filename + '_info.txt', 'w') as f:
-        f.write(str(sfreq))
-    f.close()
+        with open(filename + '_info.txt', 'w') as f:
+            f.write(str(sfreq))
+        f.close()
+    else:
+        print('Not a .edf file')
 
-edf_to_csv('S001R01.edf')
+
+#edf_to_csv('S001R01.edf')
--- tkinter_test.py
+++ tkinter_test.py
@@ -1,6 +1,8 @@
 from tkinter import *
+from tkinter import filedialog
 
 import pandas as pd
+from tqdm import tk
 
 from edf_to_csv import *
 
@@ -14,7 +16,7 @@ def retrieve():
 
 def load_csv():
     print("TEST")
-    csv_data = pd.read_csv(csv_file_path.get() + '.csv')
+    csv_data = pd.read_csv(csv_file_path.get())
 
     # need to clean header/column names (. in names causes problems, imagine will need more for other data)
     # e.g, clean_list = list of symbols to remove
@@ -40,7 +42,42 @@ def plot():
     print(f"TEST - start {start}, end {end}")
 
 
+def edf_browse():
+    path = filedialog.askopenfilename()
+    edf_file_path.delete(0, END)  # Remove current text in entry
+    edf_file_path.insert(0, path)
 
+    csv_path = path.replace('.edf', '.csv')
+
+    csv_file_path.delete(0, END)  # Remove current text in entry
+    csv_file_path.insert(0, csv_path)
+    return path
+
+
+def csv_browse():
+    path = filedialog.askopenfilename()
+    csv_file_path.delete(0, END)  # Remove current text in entry
+    csv_file_path.insert(0, path)
+    return path
+
+
+"""def on_time_change(*args): not working, will attempt again later
+    start = start_time.get()
+    end = end_time.get()
+    amount = time_amount.get()
+
+    new_start = amount - end
+    new_end = start + amount
+    new_amount = end - start
+
+    start_time.delete(0, END)
+    start_time.insert(0, new_start)
+
+    end_time.delete(0, END)
+    end_time.insert(0, new_end)
+
+    time_amount.delete(0, END)
+    time_amount.insert(0, new_amount)"""
 
 root = Tk()
 root.geometry("500x500")
@@ -50,15 +87,21 @@ frame.pack()
 
 # loading files, get way to verify filetype
 edf_file_path = Entry(frame, width=20)
-edf_file_path.insert(0, 'filename') ### change to file select
+edf_file_path.insert(0, 'EDF File')
 edf_file_path.pack(padx=5, pady=5)
+
+edf_file_browse = Button(frame, text="Browse", command=edf_browse)
+edf_file_browse.pack(padx=5, pady=5)
 
 convert_button = Button(frame, text="Convert", command=retrieve)
 convert_button.pack(padx=5, pady=5)
 
 csv_file_path = Entry(frame, width=20)
-csv_file_path.insert(0, edf_file_path.get()) ### change to file select, if converted before get filename from that
+csv_file_path.insert(0, edf_file_path.get())
 csv_file_path.pack(padx=5, pady=5)
+
+csv_file_browse = Button(frame, text="Browse", command=csv_browse)
+csv_file_browse.pack(padx=5, pady=5)
 
 load_csv_button = Button(frame, text="Load", command=load_csv)
 load_csv_button.pack(padx=5, pady=5)
@@ -76,13 +119,16 @@ start_time = Entry(frame, width=20)
 start_time.insert(0, 'start')
 start_time.pack(padx=5, pady=5)
 
+
 end_time = Entry(frame, width=20)
 end_time.insert(0, 'end')
 end_time.pack(padx=5, pady=5)
 
+
 time_amount = Entry(frame, width=20)
 time_amount.insert(0, 'amount')
 time_amount.pack(padx=5, pady=5)
+
 
 #plot button
 
